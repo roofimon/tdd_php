4a2b1c359f91b43ef5fea7bdef53392842d23062
roofimon <roofimon@gmail.com>
2016-03-29 06:12:05 +0700

    Add basic slim application

---
+++ src/index.php
@@ -0,0 +1,27 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+// Prepare the Pimple dependency injection container.
+$container = new \Slim\Container();
+
+// Add a Twig service to the container.
+$container['captcha'] = function($container) {
+  return new Captcha(2, 1, 1, 1);
+};
+
+// Create the Slim application using our container.
+$app = new \Slim\App($container);
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    //var_dump($this);
+    $name = $request->getAttribute('name');
+    $captcha = $this->captcha;
+    $left = $captcha->getLeftOperand();
+    $response->getBody()->write("Hello, $left");
+    return $response;
+});
+
+$app->run();
